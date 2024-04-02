06a7216fc3d8350a234279de8ac4caef2cdd614d
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2024-04-02 16:59:48 -0300

    criado componente icon-button

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,4 +1,5 @@
 import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight } from "lucide-react"
+import { IconButton } from "./icon-button"
 
 export function AttendeeList() {
     return (
@@ -43,9 +44,9 @@ export function AttendeeList() {
                                     <td className="py-3 px-4 text-sm text-zinc-300">7 dias atrás</td>
                                     <td className="py-3 px-4 text-sm text-zinc-300">3 dias atrás</td>
                                     <td className="py-3 px-4 text-sm text-zinc-300">
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        <IconButton transparent>
                                             <MoreHorizontal className="size-4" />
-                                        </button>
+                                        </IconButton>
                                     </td>
                                 </tr>
                             )
@@ -59,22 +60,23 @@ export function AttendeeList() {
                             <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
                                 <div className="inline-flex items-center gap-8">
                                     <span>Página 1 de 23</span>
+
                                     <div className="flex gap-1.5">
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        <IconButton>
                                             <ChevronsLeft className="size-4" />
-                                        </button>
+                                        </IconButton>
 
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        <IconButton>
                                             <ChevronLeft className="size-4" />
-                                        </button>
+                                        </IconButton>
 
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        <IconButton>
                                             <ChevronRight className="size-4" />
-                                        </button>
+                                        </IconButton>
 
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        <IconButton>
                                             <ChevronsRight className="size-4" />
-                                        </button>
+                                        </IconButton>
                                     </div>
                                 </div>
 
